2b6bbae74f01f2d8325c06666e41f45ff3c17dd4
Bryce Choquer <bryce@choquercreative.com>
2026-01-03 12:07:07 -0800

    Initial Commit

---
+++ Stride/StrideApp.swift
@@ -0,0 +1,17 @@
+//
+//  StrideApp.swift
+//  Stride
+//
+//  Created by Bryce Choquer on 2026-01-03.
+//
+
+import SwiftUI
+
+@main
+struct StrideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
